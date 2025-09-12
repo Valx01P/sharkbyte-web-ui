8f84988c932504bbad19fb9ce35d7ce535c51ed5
Pablo Valdes <pablovaldes0925@gmail.com>
2025-09-12 15:54:08 -0400

    chore(sponsors): add redbull as sponsor

--- src/app/(public_site)/pages/Sponsors.jsx
+++ src/app/(public_site)/pages/Sponsors.jsx
@@ -15,31 +15,40 @@ const Sponsors = () => {
       <div className="w-full max-w-lg relative flex justify-center items-center flex-col mb-8">
         {/* SPONSORS GRID */}
         <div className="flex flex-wrap justify-center items-center gap-2 relative py-4 z-10 mb-4 max-[650px]:mb-2 max-w-full">
-          <a href="https://www.mdc.edu/north/" target="_blank" rel="noopener noreferrer" className="flex justify-center items-center border-3 border-gray-600 pixel-shadow bg-gradient-to-br from-white to-gray-300 py-2 min-h-[230px] min-w-[230px] max-[800px]:min-w-[200px] max-[800px]:min-h-[200px] max-[700px]:min-w-[130px] max-[700px]:min-h-[130px] flex-shrink-0">
+          <a href="https://www.mdc.edu/north/" target="_blank" rel="noopener noreferrer" className="flex justify-center items-center border-3 border-gray-600 pixel-shadow bg-gradient-to-br from-white to-gray-300 py-2 min-h-[230px] min-w-[230px] max-[800px]:min-w-[200px] max-[800px]:min-h-[200px] max-[700px]:min-w-[140px] max-[700px]:min-h-[140px] flex-shrink-0">
             <Image
               src={"https://res.cloudinary.com/dqo1uzz0i/image/upload/v1757480616/mdc_ijuvcd.png"}
               height={140}
               width={140}
               alt="mdc_north_logo"
-              className="max-[800px]:w-[120px] max-[700px]:w-[100px] transition-transform duration-300 hover:scale-105"
+              className="max-[800px]:w-[120px] max-[700px]:w-[100px] transition-transform duration-300 hover:scale-105 max-[700px]:hover:scale-100"
             />
           </a>
-          <a href="https://www.mdc.edu/entec/" target="_blank" rel="noopener noreferrer" className="flex justify-center items-center border-3 border-gray-600 pixel-shadow bg-gradient-to-br from-white to-gray-300 py-2 min-h-[230px] min-w-[230px] max-[800px]:min-w-[200px] max-[800px]:min-h-[200px] max-[700px]:min-w-[130px] max-[700px]:min-h-[130px] max-[700px]:py-0 flex-shrink-0">
+          <a href="https://www.mdc.edu/entec/" target="_blank" rel="noopener noreferrer" className="flex justify-center items-center border-3 border-gray-600 pixel-shadow bg-gradient-to-br from-white to-gray-300 py-2 min-h-[230px] min-w-[230px] max-[800px]:min-w-[200px] max-[800px]:min-h-[200px] max-[700px]:min-w-[140px] max-[700px]:min-h-[140px] max-[700px]:py-0 flex-shrink-0">
             <Image
               src={"https://res.cloudinary.com/dqo1uzz0i/image/upload/v1757480615/entec_tjnwm3.png"}
               height={140}
               width={140}
               alt="entec_mdc_logo"
-              className="max-[800px]:w-[120px] max-[700px]:w-[100px] transition-transform duration-300 hover:scale-105"
+              className="max-[800px]:w-[120px] max-[700px]:w-[100px] transition-transform duration-300 hover:scale-105 max-[700px]:hover:scale-100"
             />
           </a>
-          <a href="https://www.roocapital.com/" target="_blank" rel="noopener noreferrer" className="flex justify-center items-center border-3 border-gray-600 pixel-shadow bg-gradient-to-br from-white to-gray-300 py-2 min-h-[230px] min-w-[230px] max-[800px]:min-w-[200px] max-[800px]:min-h-[200px] max-[700px]:min-w-[130px] max-[700px]:min-h-[130px] max-[700px]:py-0 flex-shrink-0">
+          <a href="https://www.roocapital.com/" target="_blank" rel="noopener noreferrer" className="flex justify-center items-center border-3 border-gray-600 pixel-shadow bg-gradient-to-br from-white to-gray-300 py-2 min-h-[230px] min-w-[230px] max-[800px]:min-w-[200px] max-[800px]:min-h-[200px] max-[700px]:min-w-[135px] max-[700px]:min-h-[135px] max-[700px]:py-0 flex-shrink-0">
             <Image
               src={"https://res.cloudinary.com/dqo1uzz0i/image/upload/v1757480616/roo_capital_zqae68.png"}
               height={190}
               width={190}
               alt="roo_capital_logo"
-              className="max-[800px]:w-[160px] max-[800px]:h-[140px] max-[700px]:w-[130px] max-[700px]:h-[130px] transition-transform duration-300 hover:scale-110"
+              className="max-[800px]:w-[160px] max-[800px]:h-[140px] max-[700px]:w-[120px] max-[700px]:h-[120px] transition-transform duration-300 hover:scale-110 max-[700px]:hover:scale-100"
+            />
+          </a>
+          <a href="https://www.redbull.com/" target="_blank" rel="noopener noreferrer" className="flex justify-center items-center border-3 border-gray-600 pixel-shadow bg-gradient-to-br from-white to-gray-300 py-2 min-h-[230px] min-w-[230px] max-[800px]:min-w-[200px] max-[800px]:min-h-[200px] max-[700px]:min-w-[140px] max-[700px]:min-h-[135px] max-[700px]:py-0 flex-shrink-0">
+            <Image
+              src={"https://res.cloudinary.com/dqo1uzz0i/image/upload/v1757706321/redbull_ouhiwo.svg"}
+              height={190}
+              width={190}
+              alt="roo_capital_logo"
+              className="max-[800px]:w-[160px] max-[800px]:h-[140px] max-[700px]:w-[120px] max-[700px]:h-[120px] transition-transform duration-300 hover:scale-110 max-[700px]:hover:scale-100"
             />
           </a>
         </div>
